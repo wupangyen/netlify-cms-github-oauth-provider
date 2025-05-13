eea38373e7fa724db174b64c7509c846b50defa3
P <p@PMacBook-Pro.local>
2025-05-13 18:32:25 -0500

    fix auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,13 @@
-const randomstring = require('randomstring')
+const randomstring = require('randomstring');
 
 module.exports = (oauth2) => {
-  // Authorization uri definition
-  const authorizationUri = oauth2.authorizeURL({
-    redirectURI: process.env.REDIRECT_URL,
-    scope: process.env.SCOPES || 'repo,user',
-    state: randomstring.generate(32)
-  })
-
   return (req, res, next) => {
-    res.redirect(authorizationUri)
-  }
-}
+    const authorizationUri = oauth2.authorizeURL({
+      redirect_uri: process.env.REDIRECT_URL, // use correct snake_case key
+      scope: process.env.SCOPES || 'repo,user',
+      state: randomstring.generate(32)
+    });
+
+    res.redirect(authorizationUri);
+  };
+};
