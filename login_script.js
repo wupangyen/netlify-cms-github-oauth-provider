862a6010fbea2ddc4150181913e373a53d468546
P <p@PMacBook-Pro.local>
2025-05-13 19:34:47 -0500

    change login_script logic

--- login_script.js
+++ login_script.js
@@ -1,47 +1,16 @@
-const isDev = process.env.NODE_ENV !== 'production';
-const originList = process.env.ORIGINS;
-
-if (!originList && !isDev) {
-  throw new Error('Missing ORIGINS environment variable.');
-}
-
-const origins = (originList || '').split(',');
-
-
 module.exports = (oauthProvider, message, content) => `
 <script>
 (function() {
-  function contains(arr, elem) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].indexOf('*') >= 0) {
-        const regex = new RegExp(arr[i].replaceAll('.', '\\\\.').replaceAll('*', '[\\\\w_-]+'))
-        console.log(regex)
-        if (elem.match(regex) !== null) {
-          return true;
-        }
-      } else {
-        if (arr[i] === elem) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-  function recieveMessage(e) {
-    console.log("recieveMessage %o", e)
-    if (!contains(${JSON.stringify(origins)}, e.origin.replace('https://', 'http://').replace('http://', ''))) {
-      console.log('Invalid origin: %s', e.origin);
-      return;
-    }
-    // send message to main window with da app
+  try {
     window.opener.postMessage(
       'authorization:${oauthProvider}:${message}:${JSON.stringify(content)}',
-      e.origin
-    )
+      '*'
+    );
+    window.close();
+  } catch (e) {
+    console.error('OAuth script error:', e);
+    document.body.innerText = 'Login failed. Please close this window.';
   }
-  window.addEventListener("message", recieveMessage, false)
-  // Start handshare with parent
-  console.log("Sending message: %o", "${oauthProvider}")
-  window.opener.postMessage("authorizing:${oauthProvider}", "*")
-})()
-</script>`
+})();
+</script>
+`
